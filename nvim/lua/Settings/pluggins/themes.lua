789f9c296de2d3bf158f073f6c94ed036343351b
Adrian <A01198211@tec.mx>
2023-11-05 11:01:03 -0600

    added folders

---
+++ nvim/lua/Settings/pluggins/themes.lua
@@ -0,0 +1,90 @@
+function setTransparent()
+    vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+    vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+end
+
+return {
+    {
+        "catppuccin/nvim",
+        name = "catppuccin",
+        priority = 1000,
+        config = function()
+            vim.cmd('colorscheme catppuccin-macchiato')
+        end
+
+    },
+    {
+        "projekt0n/github-nvim-theme",
+        tag = "v0.0.7",
+        -- config = function()
+        --     vim.cmd('colorscheme github_dimmed')
+        -- end
+        keys = "<leader>pc",
+    },
+
+    -- vscode
+    {
+        "Mofiqul/vscode.nvim",
+        keys = "<leader>pc",
+    },
+
+    -- tokyo night
+    {
+        "folke/tokyonight.nvim",
+
+    },
+    -- onedark
+    {
+        "navarasu/onedark.nvim",
+        -- config = function()
+        --     require('onedark').setup {
+        --
+        --         -- style = 'cool'
+        --     }
+        --     vim.cmd('colorscheme onedark')
+        --     -- setTransparent()
+        -- end
+        -- keys = "<leader>pc"
+    },
+    -- vscode
+    {
+        'Mofiqul/vscode.nvim',
+        keys = "<leader>pc"
+    },
+
+
+    -- tokyo night
+    {
+        'folke/tokyonight.nvim',
+
+        -- config = function()
+        --     vim.cmd("colorscheme tokyonight-night")
+        --     -- setTransparent()
+        -- end
+        keys = "<leader>pc"
+    },
+    {
+        'Mofiqul/dracula.nvim',
+        -- config = function()
+        --     vim.cmd('colorscheme dracula')
+        -- end,
+        keys = "<leader>pc"
+    },
+    -- nord
+    {
+        'shaunsingh/nord.nvim',
+        -- keys = "<leader>pc",
+    },
+    {
+        "ellisonleao/gruvbox.nvim",
+        config = function()
+            --vim.cmd('colorscheme gruvbox')
+        end,
+        keys = "<leader>pc",
+    },
+
+    {
+        "rose-pine/neovim",
+        name = "rose-pine",
+    },
+}
