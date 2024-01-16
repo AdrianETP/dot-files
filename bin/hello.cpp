e210659bb3db109d87833d2a3fd7c2c00966c478
adrian <eduardotrevinop@yahoo.com.mx>
2024-01-16 11:45:36 -0600

    all changes

---
+++ bin/hello.cpp
@@ -0,0 +1,10 @@
+#include <cstdlib>
+#include <iostream>
+#include <list>
+#include <vector>
+using namespace std;
+
+int main() {
+  cout << "hello world";
+  return 0;
+}
---
+++ bin/runcppwin.sh
@@ -0,0 +1,4 @@
+echo "your file is running ..."
+g++ $1.cpp
+
+./$1.exe
