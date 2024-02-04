f363478cda1292ab7a3fbedf1d624af683d170d4
adrian <eduardotrevinop@yahoo.com.mx>
2024-02-04 17:02:35 -0600

    screen layout

--- allstarter-linux.sh
+++ allstarter-linux.sh
@@ -18,6 +18,8 @@ curl -sS https://starship.rs/install.sh | sh
 
 # installing apps with apt
 sudo apt install build-essential
+
+cp ~/.config/.screenlayout ~
 # brave
 sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
 echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg] https://brave-browser-apt-release.s3.brave.com/ stable main"|sudo tee /etc/apt/sources.list.d/brave-browser-release.list
